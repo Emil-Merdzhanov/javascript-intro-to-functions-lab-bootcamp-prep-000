66932e06b006e26787799a2c41d504844326e11a
decent-port-4955 <emil@georgetownoliveoil.com>
2019-08-23 03:24:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
 function shout(string) {
   return string.toUpperCase()
 }
-
-function shout(whisper) {
-  return whisper.toUpperCase()
-  var uppercase = "HELLO!"
- 
-uppercase.toUpperCase() === uppercase
-}
