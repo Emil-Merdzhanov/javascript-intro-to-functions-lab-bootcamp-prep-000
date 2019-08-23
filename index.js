836dd2bff6e51b1ad4dba44d75a6f898704e5b38
decent-port-4955 <emil@georgetownoliveoil.com>
2019-08-23 03:59:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,17 @@ function logWhisper(strings) {
   console.log( strings.toLowerCase());
 }
 
-function sayHiToGrandma(string) {
-  if (string.toLowerCase(string) === string)
-    return cantUnswer;"I can't hear you!"
+function sayHiToGrandma (aWord) {
+  var cantUnswer = "I can't hear you!";
+  var yesUnswer = "YES INDEED!";
+  var lovUnswer = "I love you, too.";
+  if (aWord.toLowerCase(aWord) === aWord) {
+    return cantUnswer;
+  }
+  else if (aWord.toUpperCase(aWord) === aWord) {
+    return yesUnswer;
+  }
+  else if ("I love you, Grandma." === aWord) {
+    return lovUnswer
+  }
 }
