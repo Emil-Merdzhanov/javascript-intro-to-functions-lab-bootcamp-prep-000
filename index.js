a1ba700d2339f5ee8391ec6dc42e4e918294cd52
decent-port-4955 <emil@georgetownoliveoil.com>
2019-08-23 03:49:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ function logWhisper(strings) {
   console.log( strings.toLowerCase());
 }
 
-function sayHiToGrandma(strings) {
-  return strings.toLowerCase();
+function sayHiToGrandma(aWord) {
+  return aWord.toLowerCase();
 }
